e0448de58e6f21c921acb1f7847733952f81626f
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2023-10-07 22:31:07 -0600

    token actualizacion

--- Token.java
+++ Token.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.analiza;
+
 
 /**
  *
  * @author Daniel
  */
+
+ package com.mycompany.analiza;
 public class Token {
 
     final TipoToken tipo;
