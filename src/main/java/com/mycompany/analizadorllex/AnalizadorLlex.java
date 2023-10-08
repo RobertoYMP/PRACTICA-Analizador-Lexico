171aee1a0857b1d2611435ae9c1b0d754253b265
yahir pedraza <yahirpedraza-26@hotmail.com>
2023-10-07 20:08:16 -0600

    analizado

---
+++ src/main/java/com/mycompany/analizadorllex/AnalizadorLlex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analizadorllex;
+
+/**
+ *
+ * @author user
+ */
+public class AnalizadorLlex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
