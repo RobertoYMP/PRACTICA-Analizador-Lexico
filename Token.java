74f4b32b194b82c9eb4c99e0e2858a4a8277158b
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2023-10-07 22:17:03 -0600

    Creacion del token

---
+++ Token.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.analiza;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Token {
+
+    final TipoToken tipo;
+    final String lexema;
+    final Object literal;
+
+    public Token(TipoToken tipo, String lexema) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+        this.literal = null;
+    }
+
+    public Token(TipoToken tipo, String lexema, Object literal) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+        this.literal = literal;
+    }
+
+    public String toString() {
+        return "<" + tipo + " " + lexema + " " + literal + ">";
+    }
+}
